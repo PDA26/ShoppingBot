468dcf0ea7c2793e2ea67392f43872915dfb66ea
PDA26 <cai.xiao@northeastern.edu>
2022-04-05 00:14:13 -0400

    First day - Shell

---
+++ spider.py
@@ -0,0 +1,17 @@
+# coding = utf-8
+from bs4 import BeautifulSoup
+import re
+import sqlite3
+import urllib
+def main():
+    baseurl = "https://movie.douban.com/top250"
+    #pa
+    datalist = getData(baseurl)
+
+def getData(url):
+    res = []
+    return res
+
+if __name__ == "__main__":
+    #call
+    main()
